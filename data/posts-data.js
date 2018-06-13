9aca490ce1e56d73cc09ef733c1538be2a90ffcf
zhangyang <15210801604@163.com>
2018-06-13 23:06:39 +0800

    模板的使用

---
+++ data/posts-data.js
@@ -0,0 +1,33 @@
+var posts_content = [
+  {
+    date: "Sep 18 2016",
+    title: "正是虾肥蟹壮时",
+    imgSrc: "/images/post/crab.png",
+    avatar: "/images/avatar/1.png",
+    content: "菊黄蟹正肥，品尝秋之味，徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”，并列为秋天杭州不能缺少的事情",
+    reading: "112",
+    collection: "96"
+
+  }, {
+    date: "Nov 25 2016",
+    title: "比利·林恩的中场故事",
+    imgSrc: "/images/post/bl.png",
+    content: "李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的故事》",
+    reading: "112",
+    collect: "96",
+    avatar: "/images/avatar/2.png"
+  }, {
+    title: "当我们在谈论经济学时，我们在谈论什么？",
+    content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时经常会遇到真样的情况",
+    imgSrc: "/images/post/sls.jpg",
+    reading: 62,
+    collection: 92,
+    avatar: "/images/avatar/3.png",
+    date: "Nov 12 2016"
+
+  }
+]
+
+module.exports = {
+  postList: posts_content
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,36 +1,21 @@
+var postsData = require("../../data/posts-data.js")
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    //小程序总是会读取data对象来做数据绑定，这个动作我么称为动作A
+    //而这个动作A的执行是在onLoad事件执行之后发生的
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var posts_content=[
-      {
-        date:"Sep 18 2016",
-        title:"正是虾肥蟹壮时",
-        post_img:"/images/post/crab.png",
-        author_img:"/images/avatar/1.png",
-        content:"菊黄蟹正肥，品尝秋之味，徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”，并列为秋天杭州不能缺少的事情"
-      },{
-        date:"Nov 25 2016",
-        title:"比利·林恩的中场故事",
-        post_img:"/images/post/bl.png",
-        content:"李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的故事》",
-        view_num:"112",
-        collect_num:"96",
-        author_img:"/images/avatar/2.png"
-      }
-    ]
-
     this.setData({
-      posts_key:posts_content
+      postList: postsData.postList
     });
   },
 

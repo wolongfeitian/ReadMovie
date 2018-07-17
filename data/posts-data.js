468578df3d8cac524ae65271b98ed5160876f870
liheng27 <liheng6@jd.com>
2018-07-17 03:36:57 -0700

    s

--- data/posts-data.js
+++ data/posts-data.js
@@ -6,7 +6,8 @@ var posts_content = [
     avatar: "/images/avatar/1.png",
     content: "菊黄蟹正肥，品尝秋之味，徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”，并列为秋天杭州不能缺少的事情",
     reading: "112",
-    collection: "96"
+    collection: "96",
+    postId:0
 
   }, {
     date: "Nov 25 2016",
@@ -15,7 +16,8 @@ var posts_content = [
     content: "李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的故事》",
     reading: "112",
     collect: "96",
-    avatar: "/images/avatar/2.png"
+    avatar: "/images/avatar/2.png",
+    postId: 1
   }, {
     title: "当我们在谈论经济学时，我们在谈论什么？",
     content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时经常会遇到真样的情况",
@@ -23,7 +25,8 @@ var posts_content = [
     reading: 62,
     collection: 92,
     avatar: "/images/avatar/3.png",
-    date: "Nov 12 2016"
+    date: "Nov 12 2016",
+    postId: 2
 
   }
 ]
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,22 +1,17 @@
-var postsData = require("../../data/posts-data.js")
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    //小程序总是会读取data对象来做数据绑定，这个动作我么称为动作A
-    //而这个动作A的执行是在onLoad事件执行之后发生的
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      postList: postsData.postList
-    });
+    
   },
 
   /**
@@ -66,12 +61,5 @@ Page({
    */
   onShareAppMessage: function () {
     
-  },
-
-  onPostTap:function(event){
-    console.log(222, event.currentTarget.dataset.postid);
-    wx.navigateTo({
-      url: 'post-detail/post-detail',
-    })
   }
 })

0c5239328206af2143658580e9497ac422a03b76
liheng27 <liheng6@jd.com>
2018-07-27 06:01:30 -0700

    电影列表

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ App({
 
   globalData: {
      g_isPlayingMusic:false,
-     g_currentMusicPostId:null
+     g_currentMusicPostId:null,
+     doubanBase:"http://t.yushu.im"
    },
 
   /**
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,17 +1,72 @@
+var util = require('../../utils/utils.js')
+var app = getApp();
 Page({
+
+  //RESTFUL API JSON
+  //SOAP XML
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    inTheaters:{},
+    comingSoon:{},
+    top250:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var inTheatersUrl = app.globalData.doubanBase+ "/v2/movie/in_theaters"+"?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
     
+    this.getMovieListData(inTheatersUrl,"inTheaters","正在热映");
+    this.getMovieListData(comingSoonUrl, "comingSoon", "即将热映");
+    this.getMovieListData(top250Url, "top250","豆瓣Top250");
+  },
+
+  getMovieListData: function (url, settedKey, categoryTitle){
+    var that = this;
+    wx.request({
+      url: url,
+      data: {},
+      method: 'GET',
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        console.log('222', res);
+        that.processDoubanData(res.data, settedKey, categoryTitle);
+      }
+    })
+  },
+
+  processDoubanData: function (moviesDouban, settedKey, categoryTitle){
+    var movies = [];
+    for(var idx in moviesDouban.subjects){
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if(title.length >= 6){
+        title = title.substring(0,6)+"..."
+      }
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId:subject.id
+      }
+      movies.push(temp)
+    }
+    console.log('movies', movies);
+    var readyData ={};
+    readyData[settedKey] = {
+      movies,
+      categoryTitle
+    };
+    this.setData(readyData)
   },
 
   /**
